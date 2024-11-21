091d7c8b8cce9d784f1421d0d3bd78321532441b
Giang Pham <phamthugiang2604@gmail.com>
2024-11-21 20:57:28 +0100

    ref and validation

--- src/components/stage-1.js
+++ src/components/stage-1.js
@@ -1,10 +1,60 @@
-import React from 'react';
+import React, {useState, useContext, useRef} from 'react';
+import {Button, Form, Alert} from 'react-bootstrap';
+
+import {Context} from '../context';
 
 export const Stage1 = () => {
+    const textInput = useRef();
+    const context = useContext(Context);
+    const [error, setError] = useState([false, '']);
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+
+        const value = textInput.current.value;
+        const validate = validateInput(value);
+
+        if (validate) {
+            console.log(value);
+        } else {
+            console.log('error');
+        }
+
+    }
+
+    const validateInput = (value) => {
+        if(value === '') {
+            setError([true, 'Sorry, you need to add something']);
+
+            return false;
+        }
+
+        if (value.length <=2) {
+            setError([true, 'Sorry, you need at least 3 characters']);
+
+            return false
+        }
+
+        return true;
+    }
+
     return (
-        <div className='App'>
-            Stage 1
-        </div>
+        <>
+            <Form className='mt-4' onSubmit={handleSubmit}>
+                <Form.Group>
+                    <Form.Control
+                        type='text'
+                        placeholder='Add player name'
+                        name='player'
+                        ref={textInput}
+                    />
+                </Form.Group>
+
+                <Button className='miami' variant='primary' type='submit'>
+                    Add player
+                </Button>
+            </Form>
+        </>
     );
 }
 
